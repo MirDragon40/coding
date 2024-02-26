01ae52b5492d96dd759d2d69906430843460e387
DESKTOP-358CSLP\USER <mirjeong6463@gmail.com>
2024-02-26 17:36:57 +0900

    델리게이트 하는 중 -> 람다표현식 학습

--- Assets/02_Scripts/Class/Delegate/FuncExample.cs
+++ Assets/02_Scripts/Class/Delegate/FuncExample.cs
@@ -11,12 +11,18 @@ public class FuncExample : MonoBehaviour
         // 반드시 반환값이 있고, 매개 변수의 개수를 0~16개인 메서드를 담을 수 있는 대리자
         Func<int, bool> a = IsOdd;
         Debug.Log(a(17));
-    }
 
-    // 홀수 판별 함수
-    bool IsOdd(int num)
-    {
-        return num % 2 == 1;
-    }
+        // 람다표현식을 이용한 방식
+        Func<int, bool> b = (num) =>
+        {
+            return num % 2 == 1;
+        };   // 중괄호는 생략이 가능
 
+        // 홀수 판별 함수
+        bool IsOdd(int num)
+        {
+            return num % 2 == 1;
+        }
+
+    }
 }
